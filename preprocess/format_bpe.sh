8c9fd0306efd52404e92e9f993b3ef5da12cee7c
chenchuyi <chenchuyi@xuetangx.com>
2020-07-14 23:15:51 +0100

    BPEmb

--- preprocess/format_bpe.sh
+++ preprocess/format_bpe.sh
@@ -14,13 +14,13 @@ do
     targetDir=../data/${lang}-${merge_N}-bpe-${n_context}-context
     mkdir -p ${targetDir}
 
-    python3 bpe-format.py $UD_directory/UD_${lang}/dev ${lang} dev ${merge_N} ${n_context}
+    python3 format-bpe.py $UD_directory/UD_${lang}/dev ${lang} dev ${merge_N} ${n_context}
     mv dev-* ${targetDir}/.
 
-    python3 bpe-format.py $UD_directory/UD_${lang}/train ${lang} train ${merge_N} ${n_context}
+    python3 format-bpe.py $UD_directory/UD_${lang}/train ${lang} train ${merge_N} ${n_context}
     mv train-* ${targetDir}/.
 
-    python3 bpe-format.py $UD_directory/UD_${lang}/test ${lang} test ${merge_N} ${n_context}
+    python3 format-bpe.py $UD_directory/UD_${lang}/test ${lang} test ${merge_N} ${n_context}
     mv test-* ${targetDir}/.
 done
 
