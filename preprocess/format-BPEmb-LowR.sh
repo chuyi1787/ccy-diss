5d66555a99fc82b49e41951ee23fa9c10942c6f6
chenchuyi <chenchuyi@xuetangx.com>
2020-07-30 23:16:40 +0100

    lowresource format.sh

---
+++ preprocess/format-BPEmb-LowR.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+merge_N=$1 #500
+n_context=$2 #20
+train_token_Nk=10
+dev_token_Nk=3
+
+UD_directory=selectedUDT-v2.1 # origianl data sources
+languages="English Arabic Turkish Spanish" # list of languages to process
+
+
+mkdir -p ../data-lowresource
+
+for lang in ${languages}
+do
+    targetDir=../data-lowresource/${lang}-${merge_N}-BPEmb-${n_context}-context-lowR
+    mkdir -p ${targetDir}
+
+    python3 format-BPEmb.py $UD_directory/UD_${lang}/dev ${lang} dev ${merge_N} ${n_context} ${dev_token_Nk}
+    mv dev-* ${targetDir}/.
+
+    python3 format-BPEmb.py $UD_directory/UD_${lang}/train ${lang} train ${merge_N} ${n_context} ${train_token_Nk}
+    mv train-* ${targetDir}/.
+
+    python3 format-BPEmb.py $UD_directory/UD_${lang}/test ${lang} test ${merge_N} ${n_context}
+    mv test-* ${targetDir}/.
+done
+
--- preprocess/format-char-LowR.sh
+++ preprocess/format-char-LowR.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 N=( $1 ) # N - left and N-right sentence characters to extract
-MAX_token_Nk=$2 #10
+train_token_Nk=10
+dev_token_Nk=3
 
 UD_directory=selectedUDT-v2.1 #change for other versions
 languages="English Arabic Turkish Spanish" # list of languages to process
@@ -14,16 +15,16 @@ for n in "${N[@]}"
 do
     for lang in ${languages}
     do
-        targetDir=../data-lowresource/${lang}-${N}-char-context-${MAX_token_Nk}k
+        targetDir=../data-lowresource/${lang}-${N}-char-context-lowR
         mkdir -p ${targetDir}
 
-        python3 format-char.py $UD_directory/UD_${lang}/dev ${lang} dev ${n} ${MAX_token_Nk}
+        python3 format-char.py $UD_directory/UD_${lang}/dev ${lang} dev ${n} ${dev_token_Nk}
         mv dev-* ${targetDir}/.
 
-        python3 format-char.py $UD_directory/UD_${lang}/train ${lang} train ${n} ${MAX_token_Nk}
+        python3 format-char.py $UD_directory/UD_${lang}/train ${lang} train ${n} ${train_token_Nk}
         mv train-* ${targetDir}/.
 
-        python3 format-char.py $UD_directory/UD_${lang}/test ${lang} test ${n} ${MAX_token_Nk}
+        python3 format-char.py $UD_directory/UD_${lang}/test ${lang} test ${n}
         mv test-* ${targetDir}/.
     done
 done
