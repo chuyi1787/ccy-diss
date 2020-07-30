738a9d745e401ba8fd2b983d4df3ae5f98906dc1
chenchuyi <chenchuyi@xuetangx.com>
2020-07-30 02:44:40 +0100

    val-acc

--- val-acc.sh
+++ val-acc.sh
@@ -2,9 +2,12 @@
 
 models=$1 # models / models-10k/ models-char
 
+rm val-acc-${models}.res
+
 for dir in ${models}/*  #dir -  the dir name under models/
 do
   refPath=data/${dir##*/}/dev-targets
   echo "++++++++"${dir} >> val-acc-${models}.res
   python3 accuracy.py $refPath ${dir}/dev_pred.txt >> val-acc-${models}.res
+
 done
