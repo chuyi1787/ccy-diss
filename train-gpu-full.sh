7fdf6ce07c2c2aee962ed64a1cf48c4f6f553fd4
chenchuyi <chenchuyi@xuetangx.com>
2020-08-02 14:24:09 +0100

    delete uselss

--- train-gpu-full.sh
+++ train-gpu-full.sh
@@ -1,7 +1,9 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-type=$1 #25-char-context-10k
-languages=$2
+modelDir=$1 #models-bpe/ models-bpeall/ models-char
+type=$2 #500-bpe-20-context
+languages=$3
+
 
 batch_size=60
 burn_in_for_n_epochs=10
@@ -12,9 +14,9 @@ val_every_n_epochs=1
 for lang in ${languages}
 do
 datadir=${lang}-${type}
-mkdir -p models/${lang}-${type}/
+mkdir -p ${modelDir}/${lang}-${type}/
 
-steps_of_an_epoch=($(wc -l ./data/${datadir}/train-sources))
+steps_of_an_epoch=($(wc -l ./data-full/${datadir}/train-sources))
 #use the first 10 epochs as a burn-in period
 validBurnIn=$((steps_of_an_epoch *${burn_in_for_n_epochs} / ${batch_size}))
 # early stopping with patience 10
@@ -24,9 +26,12 @@ val_steps=$((steps_of_an_epoch *${val_every_n_epochs}/ ${batch_size}))
 
 echo "Sarting training, steps_of_an_epoch:"
 echo ${val_steps}
+echo "validBurnIn steps"
+echo ${validBurnIn}
 
-onmt_train -data data/${datadir}/data-pp/${datadir}\
-  --save_model models/${datadir}/${lang}-${type}\
+
+onmt_train -data data-full/${datadir}/data-pp/${datadir}\
+  --save_model ${modelDir}/${datadir}/${lang}-${type}\
   --save_checkpoint_steps ${steps_of_an_epoch}\
   --keep_checkpoint 3\
   --encoder_type brnn\
@@ -44,8 +49,8 @@ onmt_train -data data/${datadir}/data-pp/${datadir}\
   --valid_steps ${val_steps}\
   --warmup_steps ${validBurnIn}\
   --train_steps 3000000\
-  --report_every ${val_steps} \
-  --gpu_ranks 0 2>&1 | tee models/${datadir}/train-gpu.txt
+  --report_every ${steps_of_an_epoch} \
+  --gpu_ranks 0 2>&1 | tee ${modelDir}/${datadir}/train-log-${lang}-${type}.txt
 echo "End of training"
 
 
