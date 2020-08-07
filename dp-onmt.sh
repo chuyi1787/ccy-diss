e321249dab9485666c4ba770350e959ffe4c3b85
chenchuyi <chenchuyi@xuetangx.com>
2020-08-07 20:22:21 +0100

    dp

--- dp-onmt.sh
+++ dp-onmt.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-type=$1 #500-BPEmb-20-context #20-char-context
-languages="English Arabic Turkish Spanish"
+type=$1 #500-BPEmb-20-context char-20-context trigram-15-context
+mode=$2 #"lite"
+datadir=data-${mode}
+echo $datadir
+languages="English Arabic Turkish Spanish Indonesian"
 
-mkdir -p ./models
 for lang in ${languages}
 do
 echo ${lang}-${type}
-mkdir -p data/${lang}-${type}/data-pp/
+mkdir -p ${datadir}/${lang}-${type}/data-pp/
 
 
 echo "Starting dp"
 onmt_preprocess \
-  -train_src data-lowresource/${lang}-${type}/train-sources\
-  -train_tgt data-lowresource/${lang}-${type}/train-targets \
-  -valid_src data-lowresource/${lang}-${type}/dev-sources \
-  -valid_tgt data-lowresource/${lang}-${type}/dev-targets \
+  -train_src ${datadir}/${lang}-${type}/train-sources\
+  -train_tgt ${datadir}/${lang}-${type}/train-targets \
+  -valid_src ${datadir}/${lang}-${type}/dev-sources \
+  -valid_tgt ${datadir}/${lang}-${type}/dev-targets \
   -src_seq_length 75 \
   -tgt_seq_length 75 \
-  -save_data data-lowresource/${lang}-${type}/data-pp/${lang}-${type}
+  -save_data ${datadir}/${lang}-${type}/data-pp/${lang}-${type}
 echo "End of dp"
 
 done
